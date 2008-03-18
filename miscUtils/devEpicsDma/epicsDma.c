f469a126eda479bbd7f48fc8c8479b9fbba6ea74
Stephanie Allison <saa@slac.stanford.edu>
2008-03-18 16:49:53 +0000

    Add hooks for new RTEMS DMA support provided by Till.

--- miscUtils/devEpicsDma/epicsDma.c
+++ miscUtils/devEpicsDma/epicsDma.c
@@ -22,31 +22,46 @@
 /*
  * Don't cause linker errors if BSP fails to supply these routines
  */
-#ifndef HAS_UNIVERSEDMA
+#ifdef HAS_UNIVERSEDMA
+#include <drvUniverseDma.h>
+#elif defined( __rtems__) && defined(HAS_RTEMSDMASUP)
+#include <drvRTEMSDmaSup.h>
+#else
 #ifndef vxWorks
 typedef void (*VOIDFUNCPTR)(void *);
 typedef unsigned long   UINT32;
 #endif
 typedef struct dmaRequest *DMA_ID;
+#endif
+typedef DMA_ID (*sysDmaCreateFunc)(VOIDFUNCPTR callback, void *context);
+typedef int (*sysDmaStatusFunc)(DMA_ID dmaId);
+typedef int (*sysDmaToVmeFunc)(DMA_ID dmaId, UINT32 vmeAddr, int adrsSpace,
+              void *pLocal, int length, int dataWidth);
+typedef int (*sysDmaFromVmeFunc)(DMA_ID dmaId, void *pLocal, UINT32 vmeAddr,
+              int adrsSpace, int length, int dataWidth);
+#ifdef HAS_UNIVERSEDMA
+static sysDmaCreateFunc  psysDmaCreate  = universeDmaCreate;
+static sysDmaStatusFunc  psysDmaStatus  = universeDmaStatus;
+static sysDmaFromVmeFunc psysDmaFromVme = universeDmaFromVme;
+static sysDmaToVmeFunc   psysDmaToVme   = universeDmaToVme;
+#elif defined(__rtems__) && defined(HAS_RTEMSDMASUP)
+static sysDmaCreateFunc  psysDmaCreate  = rtemsVmeDmaCreate;
+static sysDmaStatusFunc  psysDmaStatus  = rtemsVmeDmaStatus;
+static sysDmaFromVmeFunc psysDmaFromVme = rtemsVmeDmaFromVme;
+static sysDmaToVmeFunc   psysDmaToVme   = rtemsVmeDmaToVme;
+#else
 DMA_ID sysDmaCreate(VOIDFUNCPTR callback, void *context) __attribute__((weak));
 int sysDmaStatus(DMA_ID dmaId) __attribute__((weak));
 int sysDmaToVme(DMA_ID dmaId, UINT32 vmeAddr, int adrsSpace,
               void *pLocal, int length, int dataWidth) __attribute__((weak));
 int sysDmaFromVme(DMA_ID dmaId, void *pLocal, UINT32 vmeAddr,
               int adrsSpace, int length, int dataWidth) __attribute__((weak));
-static DMA_ID (*psysDmaCreate)(VOIDFUNCPTR callback, void *context) = sysDmaCreate;
-static int (*psysDmaStatus)(DMA_ID dmaId) = sysDmaStatus;
-static int (*psysDmaToVme)(DMA_ID dmaId, UINT32 vmeAddr, int adrsSpace,
-              void *pLocal, int length, int dataWidth) = sysDmaToVme;
-static int (*psysDmaFromVme)(DMA_ID dmaId, void *pLocal, UINT32 vmeAddr,
-              int adrsSpace, int length, int dataWidth) = sysDmaFromVme;
-#else
-#include <drvUniverseDma.h>
-static sysDmaCreate    psysDmaCreate  = universeDmaCreate;
-static sysDmaStatus    psysDmaStatus  = universeDmaStatus;
-static sysDmaFromVme   psysDmaFromVme = universeDmaFromVme;
-static sysDmaToVme     psysDmaToVme   = universeDmaToVme;
+static sysDmaCreateFunc  psysDmaCreate  = sysDmaCreate;
+static sysDmaStatusFunc  psysDmaStatus  = sysDmaStatus;
+static sysDmaFromVmeFunc psysDmaFromVme = sysDmaFromVme;
+static sysDmaToVmeFunc   psysDmaToVme   = sysDmaToVme;
 #endif
+
 /*
  * EPICS DMA identifier
  */
